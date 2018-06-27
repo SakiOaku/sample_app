958e3e4078ccbc8c8eb464f8076ccc7c256b8505
大阿久 咲希 <saki_oaku@orc-net.local>
2018-06-27 14:39:39 +0900

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -9,8 +9,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                        password_confirmation: "bar" } }
     end
   assert_template 'users/new'
-  #assert_select 'div#<CSS id for error explanation>'
-  #assert_select 'div.<CSS class for field with error>'
+  assert_select 'div#error_explanation'
+  assert_select 'div.col-md-6'
   end
   # test "the truth" do
   #   assert true
